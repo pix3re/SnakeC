e6afb3a53f2c93cde29ace6c42562f88b0aa2153
pixe <pijuspovilas@yahoo.com>
2022-08-31 21:55:47 +0300

    Added tail logic, minor polishes

--- SnakeC/main.cpp
+++ SnakeC/main.cpp
@@ -10,48 +10,68 @@ int BOARD_HEIGHT = 25;
 
 int FRUIT_X, FRUIT_Y;
 int SNAKE_H[2] = { BOARD_WIDTH / 2, BOARD_HEIGHT / 2 };
-int SNAKE_T[4];
+int SNAKE_TX[100], SNAKE_TY[100];
+int nTAIL = 3;
 int SCORE = 0;
 
 enum SNAKE_DIRECTION { UP, DOWN, LEFT, RIGHT };
 
 SNAKE_DIRECTION currentDir = DOWN;
 
-bool initGameLoop = false;
+bool initGameLoop = true;
+bool initProgram = false;
 
 
 void displayProlog();
+void goodbayProlog();
 void drawGame();
 void handleInput();
 void handleGameLogic();
+void handleTailLogic();
+void setupGameVariables();
 void drawFruit();
 bool askQuestionYN(const std::string Question);
 void gameState(const bool Lost);
 void addScore();
 void drawScore();
+void playAgain();
 inline void clearScreen();
 
 int main()
 {
 	displayProlog();
-	initGameLoop = askQuestionYN("Do you wish to start game?");
+	initProgram = askQuestionYN("Do you wish to start game?");
 
-	if (initGameLoop)
+	while (initProgram)
 	{
-		drawFruit();
+		setupGameVariables();
+
+		while (initGameLoop)
+		{
+			clearScreen();
+			drawGame();
+			drawScore();
+			handleInput();
+			handleGameLogic();
+			Sleep(300);
+		}
+
+		playAgain();
 	}
 
-	while (initGameLoop)
-	{
-		clearScreen();
-		drawGame();
-		drawScore();
-		handleInput();
-		handleGameLogic();
-		Sleep(300);
-	}
+	goodbayProlog();
 
 	return 0;
+}
+
+void setupGameVariables()
+{
+	drawFruit();
+	SNAKE_H[0] = BOARD_WIDTH / 2;
+	SNAKE_H[1] = BOARD_HEIGHT / 2;
+	nTAIL = 3;
+	handleTailLogic();
+	SCORE = 0;
 }
 
 void displayProlog()
@@ -60,6 +80,12 @@ void displayProlog()
 	std::cout << "Basic rules is to get as many scores as possible." << std::endl;
 	std::cout << "Basic movement can be done using A, W, S, D keys." << std::endl;
 	std::cout << std::endl;
+}
+
+void goodbayProlog()
+{
+	std::cout << "Thank you for playing!" << std::endl;
+	std::cout << "We'd hope you come back soon!!" << std::endl;
 }
 
 void drawFruit()
@@ -76,7 +102,6 @@ void drawGame()
 	{
 		for (int y = 0; y <= BOARD_HEIGHT; y++)
 		{
-			//std::cout << "[" << x << "," << y << "]";
 			if (y == 0 || y == BOARD_HEIGHT || x == 0 || x == BOARD_WIDTH)
 			{
 				std::cout << "#";
@@ -91,7 +116,17 @@ void drawGame()
 			}
 			else
 			{
-				std::cout << " ";
+				bool print = false;
+				for (int k = 0; k < nTAIL; k++)
+				{
+					if (SNAKE_TX[k] == x && SNAKE_TY[k] == y)
+					{
+						print = true;
+						std::cout << "o";
+					}
+				}
+				if (!print)
+					std::cout << " ";
 			}
 		}
 		std::cout << std::endl;
@@ -105,30 +140,51 @@ void handleInput()
 		switch (_getch())
 		{
 		case 'w':
-			currentDir = UP;
-
+			if (currentDir != DOWN)
+				currentDir = UP;
 			break;
 
 		case 's':
-			currentDir = DOWN;
-
+			if (currentDir != UP)
+				currentDir = DOWN;
 			break;
 
 		case 'a':
-			currentDir = LEFT;
-
+			if (currentDir != RIGHT)
+				currentDir = LEFT;
 			break;
 
 		case 'd':
-			currentDir = RIGHT;
-
+			if (currentDir != LEFT)
+				currentDir = RIGHT;
 			break;
 		}
 	}
 }
 
+void handleTailLogic()
+{
+	int prevX = SNAKE_TX[0];
+	int prevY = SNAKE_TY[0];
+	int prevX2, prevY2;
+	SNAKE_TX[0] = SNAKE_H[0];
+	SNAKE_TY[0] = SNAKE_H[1];
+
+	for (int i = 1; i < nTAIL; i++)
+	{
+		prevX2 = SNAKE_TX[i];
+		prevY2 = SNAKE_TY[i];
+		SNAKE_TX[i] = prevX;
+		SNAKE_TY[i] = prevY;
+		prevX = prevX2;
+		prevY = prevY2;
+	}
+}
+
 void handleGameLogic()
 {
+	handleTailLogic();
+
 	if (currentDir == UP)
 	{
 		SNAKE_H[0] = SNAKE_H[0] - 1;
@@ -150,11 +206,20 @@ void handleGameLogic()
 	{
 		addScore();
 		drawFruit();
+		nTAIL++;
 	}
 
-	if (SNAKE_H[0] == 0 || SNAKE_H[0] == BOARD_WIDTH || SNAKE_H[1] == 0 || SNAKE_H[1] == BOARD_HEIGHT)
+	if (SNAKE_H[0] < 0 || SNAKE_H[1] == BOARD_WIDTH || SNAKE_H[1] == 0 || SNAKE_H[0] > BOARD_HEIGHT)
 	{
 		gameState(true);
+	}
+
+	for (int k = 0; k < nTAIL; k++)
+	{
+		if (SNAKE_TX[k] == SNAKE_H[0] && SNAKE_TY[k] == SNAKE_H[1])
+		{
+			gameState(true);
+		}
 	}
 }
 
@@ -165,7 +230,17 @@ void gameState(const bool Lost)
 		initGameLoop = false;
 		clearScreen();
 		std::cout << "YOU LOST !!!" << std::endl;
+		std::cout << "Scores managed to get: " << SCORE << std::endl;
 	}
+}
+
+void playAgain()
+{
+	bool vPlayAgain = askQuestionYN("Do you wish to play again??");
+	if (vPlayAgain)
+		initGameLoop = true;
+	else
+		initProgram = false;
 }
 
 void addScore()
